bb3cc9b53ded922dbc8325bcc8d643bdccc80b39
JEETVP <roberto.villegasrvo@gmail.com>
2025-07-30 10:58:45 -0600

    por favor ya funciona

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 ﻿const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+require('dotenv').config();
+
 const authRoutes = require('./Routes/authRoutes');
 
-dotenv.config();
 const app = express();
 app.use(express.json());
-
 app.use('/api/auth', authRoutes);
 
 // Conexión a MongoDB
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('MongoDB connected');
-    app.listen(process.env.PORT || 3000, () => {
-        console.log(`Server running on port ${process.env.PORT || 3000}`);
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log('MongoDB connected');
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
     });
-}).catch(err => console.error(err));
-
-

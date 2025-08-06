cc0371e8a1f909a71e61107e5ee902b92e7f1b17
JEETVP <roberto.villegasrvo@gmail.com>
2025-08-06 12:21:17 -0600

    rookie mistake

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -12,7 +12,5 @@ router.get('/', productController.getAllProducts);
 // Obtener un producto por ID
 router.get('/:id', productController.getProductById);
 
-// (Opcional) Eliminar un producto por ID (si quieres gestión de inventario desde backend)
-router.delete('/delete/:id', authenticate, productController.deleteProduct);
 
 module.exports = router;

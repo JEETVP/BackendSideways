c8f7372fe682967181813e8f28b546bd2f90ddc4
JEETVP <roberto.villegasrvo@gmail.com>
2025-08-05 21:06:44 -0600

    correciones en routes

--- Routes/orderRoutes.js
+++ Routes/orderRoutes.js
@@ -4,8 +4,6 @@ const orderController = require('../Controllers/OrderController');
 const authenticate = require('../Middlewares/authMiddleware'); 
 
 router.post('/create', authenticate, orderController.createOrder);
-router.get('/my-orders', authenticate, orderController.getUserOrders);
-router.get('/:id', authenticate, orderController.getOrderById);
 
 module.exports = router;
 

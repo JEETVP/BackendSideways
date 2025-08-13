1dd0f81cd53e358ce2159333523be7150bac8e3f
JEETVP <roberto.villegasrvo@gmail.com>
2025-08-13 10:48:56 -0600

    consistencia en ruta

--- Routes/orderRoutes.js
+++ Routes/orderRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const orderController = require('../Controllers/OrderController'); 
 const authenticate = require('../Middlewares/authMiddleware'); 
 
-router.post('/create', authenticate, orderController.createOrder);
+router.post('/add', authenticate, orderController.createOrder);
 router.patch('/:id/status', authenticate, /* requireAdmin, */ orderController.updateOrderStatus);
 
 module.exports = router;

2e1b9c5a8da4f73d26507cd902f66ce5ea5db8b0
JEETVP <roberto.villegasrvo@gmail.com>
2025-08-14 18:41:54 -0600

    patching typo

--- Routes/orderRoutes.js
+++ Routes/orderRoutes.js
@@ -6,10 +6,10 @@ const authenticate = require('../Middlewares/authMiddleware');
 router.post('/add', authenticate, orderController.createOrder);
 router.patch('/:id/status', authenticate, /* requireAdmin, */ orderController.updateOrderStatus);
 // Obtener todas las órdenes (solo admin)
-router.get('/all', authenticate, OrderController.getAllOrders);
+router.get('/all', authenticate, orderController.getAllOrders);
 
 // Actualizar estado de una orden (eliminar si es "Entregado")
-router.put('/:id/status', authenticate, OrderController.deleteOrderStatus);
+router.put('/:id/status', authenticate, orderController.deleteOrderStatus);
 
 
 module.exports = router;
